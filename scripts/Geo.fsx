713bfccea94f35ed9bad95c348372b84c66af14c
Max Wilson <wilson.max@gmail.com>
2022-12-15 08:32:43 -0800

    Switch to storing Ids, with support for looking up other info via API if needed for constraints

--- scripts/Geo.fsx
+++ scripts/Geo.fsx
@@ -2,48 +2,49 @@ module Geo
 #I __SOURCE_DIRECTORY__
 #load "Common.fsx"
 
-type API<'T, 'Id> =
-    abstract getId: 'T -> 'Id
+type API<'id, 'actual> =
+    abstract getActual: 'id -> 'actual
 
 //type T = string
 type Coord = float * float
-type 'id Constraint = NearThing of 'id * distance: float | NearCoord of Coord * distance: float
-type 'id Preference = CloseTo of 'id | AwayFrom of 'id
-type DiscreteGeo<'t, 'id when 'id: comparison and 't: comparison>(ms, ns, api: API<_,_>) =
+type Id = string
+type Constraint = NearThing of Id * distance: float | NearCoord of Coord * distance: float
+type Preference = CloseTo of Id | AwayFrom of Id
+type DiscreteGeo(ms, ns, api: API<_,_>) =
     let points : Coord list =
         [for m in ms do
             for n in ns do
                 m,n]
-    let mutable spaces : Map<Coord, 't list> = points |> List.map (fun p -> p, []) |> Map.ofList
-    let mutable occupancy : Map<'id, Coord> = Map.empty
-    let place (thing: 't, coords) =
+    let mutable spaces : Map<Coord, Id list> = points |> List.map (fun p -> p, []) |> Map.ofList
+    let mutable occupancy : Map<Id, Coord> = Map.empty
+    let place (thingId: Id, coords) =
         if not (points |> List.contains coords) then shouldntHappen "Out of bounds, out of scope for this scenario."
-        spaces <- spaces |> Map.change coords (function Some lst -> Some (lst@[thing]) | None -> Some [thing])
-        occupancy <- occupancy |> Map.add (api.getId thing) coords
-    let move (thing: 't, from, to1) =
+        spaces <- spaces |> Map.change coords (function Some lst -> Some (lst@[thingId]) | None -> Some [thingId])
+        occupancy <- occupancy |> Map.add (thingId) coords
+    let move (thingId: Id, from, to1) =
         if not (points |> List.contains from) then shouldntHappen "Out of bounds, out of scope for this scenario."
         elif not (points |> List.contains to1) then shouldntHappen "Out of bounds, out of scope for this scenario."
         else
-            let creature, remainder = spaces[from] |> List.partition ((=)thing)
+            let creature, remainder = spaces[from] |> List.partition ((=)thingId)
             if creature.IsEmpty then
-                shouldntHappen $"Placement error! {thing} isn't at {from} in the first place!"
+                shouldntHappen $"Placement error! {thingId} isn't at {from} in the first place!"
             spaces <- spaces
                     |> Map.add from remainder
-                    |> Map.change to1 (function Some lst -> Some (lst@[thing]) | None -> Some [thing])
+                    |> Map.change to1 (function Some lst -> Some (lst@[thingId]) | None -> Some [thingId])
     let draw() : string =
         let s = System.Text.StringBuilder()
         for m in ms do
             for n in ns do
                 match spaces |> Map.tryFind (m,n) with
-                | Some (thing::_) when (api.getId thing).ToString().Length > 0 ->
-                    s.Append ((api.getId thing).ToString().Substring(0, 1))
+                | Some (thingId::_) when thingId.Length > 0 ->
+                    s.Append (thingId.Substring(0, 1))
                 | None | Some [] | Some _ ->
                     s.Append "."
                 |> ignore
             s.AppendLine "" |> ignore
         s.ToString()
     let thingsInRegion (coords: Coord list) : 't list = notImpl()
-    let pointsNear(coord: Coord, constraints: _ Constraint list, preferences: _ Preference list) =
+    let pointsNear(coord: Coord, constraints: Constraint list, preferences: Preference list) =
         notImpl()
     member _.Place args = place args
     member _.Move args = move args
@@ -51,7 +52,8 @@ type DiscreteGeo<'t, 'id when 'id: comparison and 't: comparison>(ms, ns, api: A
 
 type Api() =
     interface API<string, string> with
-        member _.getId (s:string) = s
+        member this.getActual(id: Id): string = notImpl()
+
 let (ms, ns) = ([(1.)..0.5..(2.)], [(0.)..0.5..(20.)])
 let geo = DiscreteGeo(ms, ns, Api())
 geo.Place("Bob", (1.5, 10.))
